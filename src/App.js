d72caef66854787d85e633e05c0f6428a5099984
Saranya Vinjarapu <saranya.vinjarapu@gmail.com>
2021-02-09 22:00:21 +0530

    created Header component and added the component to app container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import Header from "./components/Header/Header";
+
 function App() {
-  return <div className="App">Countries Display</div>;
+  return (
+    <div className="main-container">
+      <ToastContainer />
+      <Header />
+    </div>
+  );
 }
 
 export default App;
