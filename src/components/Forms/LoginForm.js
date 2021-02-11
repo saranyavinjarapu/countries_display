35b5bac0067db4666b19866cf0679076146d0efa
Saranya Vinjarapu <saranya.vinjarapu@gmail.com>
2021-02-11 14:48:37 +0530

    added logic for form validation

--- src/components/Forms/LoginForm.js
+++ src/components/Forms/LoginForm.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Forms.css";
 
+/* eslint-disable */
+const validEmailRegex = RegExp(
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+);
+
 function LoginForm() {
+  const [emailErrors, setEmailErrors] = useState([]);
+  const [passwordErrors, setPasswordErrors] = useState([]);
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.currentTarget;
+
+    let email_error_message, password_error_message;
+
+    switch (name) {
+      case "email":
+        email_error_message =
+          validEmailRegex.test(value) && value.length > 0
+            ? ""
+            : "Email is not valid!";
+        break;
+      case "password":
+        password_error_message =
+          value.length < 8 ? "Password must be 8 characters long!" : "";
+        break;
+      default:
+        break;
+    }
+
+    setEmailErrors(email_error_message);
+    setPasswordErrors(password_error_message);
+  };
+
   return (
     <form className="login_form">
       <h3>Login using email</h3>
@@ -11,18 +44,26 @@ function LoginForm() {
         <label>Email address</label>
         <input
           type="email"
+          name="email"
           className="form-control"
           placeholder="Enter email"
+          onChange={handleChange}
         />
+
+        <span className="error">{emailErrors}</span>
       </div>
 
       <div className="form-group">
         <label>Password</label>
         <input
           type="password"
+          name="password"
           className="form-control"
           placeholder="Enter password"
+          onChange={handleChange}
+          noValidate
         />
+        <span className="error">{passwordErrors}</span>
       </div>
 
       <div className="form-group">
@@ -30,16 +71,20 @@ function LoginForm() {
           <input
             type="checkbox"
             className="custom-control-input"
-            id="customCheck1"
+            id="custom_check"
           />
-          <label className="custom-control-label" htmlFor="customCheck1">
+          <label className="custom-control-label" htmlFor="custom_check">
             Remember me
           </label>
         </div>
       </div>
-
       <Link to="/countries">
-        <button type="submit" className="btn btn-primary btn-block">
+        <button
+          type="submit"
+          className="btn btn-primary btn-block"
+          disabled={emailErrors || passwordErrors}
+        >
+          {" "}
           Submit
         </button>
       </Link>
--- src/components/Forms/SignupForm.js
+++ src/components/Forms/SignupForm.js
@@ -1,48 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Forms.css";
 import { Link } from "react-router-dom";
 
+/* eslint-disable */
+const validEmailRegex = RegExp(
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+);
+
 function SignupForm() {
+  const [firstNameErrors, setFirstNameErrors] = useState([]);
+  const [lastNameErrors, setLastNameErrors] = useState([]);
+  const [emailErrors, setEmailErrors] = useState([]);
+  const [passwordErrors, setPasswordErrors] = useState([]);
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.currentTarget;
+
+    let fname_err, lname_err, email_err, password_err;
+
+    switch (name) {
+      case "first_name":
+        fname_err =
+          value.length < 5 ? "First Name must be 5 characters long!" : "";
+        break;
+      case "last_name":
+        lname_err =
+          value.length < 5 ? "Last Name must be 5 characters long!" : "";
+        break;
+      case "email":
+        email_err =
+          validEmailRegex.test(value) && value.length > 0
+            ? ""
+            : "Email is not valid!";
+        break;
+      case "password":
+        password_err =
+          value.length < 8 ? "Password must be 8 characters long!" : "";
+        break;
+      default:
+        break;
+    }
+
+    setFirstNameErrors(fname_err);
+    setLastNameErrors(lname_err);
+    setEmailErrors(email_err);
+    setPasswordErrors(password_err);
+  };
+
   return (
     <form className="signup_form">
       <h3>Signup Using Email</h3>
 
       <div className="form-group">
         <label>First name</label>
-        <input type="text" className="form-control" placeholder="First name" />
+        <input
+          type="text"
+          name="first_name"
+          className="form-control"
+          placeholder="First name"
+          onChange={handleChange}
+        />
+        <span className="error">{firstNameErrors}</span>
       </div>
 
       <div className="form-group">
         <label>Last name</label>
-        <input type="text" className="form-control" placeholder="Last name" />
+        <input
+          type="text"
+          name="last_name"
+          className="form-control"
+          placeholder="Last name"
+          onChange={handleChange}
+        />
+        <span className="error">{lastNameErrors}</span>
       </div>
 
       <div className="form-group">
         <label>Email address</label>
         <input
           type="email"
+          name="email"
           className="form-control"
           placeholder="Enter email"
+          onChange={handleChange}
         />
+        <span className="error">{emailErrors}</span>
       </div>
 
       <div className="form-group">
         <label>Password</label>
         <input
           type="password"
+          name="password"
           className="form-control"
           placeholder="Enter password"
+          onChange={handleChange}
         />
+        <span className="error">{passwordErrors}</span>
       </div>
 
       <Link to="/countries">
-        <button type="submit" className="btn btn-primary btn-block">
+        <button
+          type="submit"
+          className="btn btn-primary btn-block"
+          disabled={
+            firstNameErrors || lastNameErrors || emailErrors || passwordErrors
+          }
+        >
           Submit
         </button>
       </Link>
 
       <p className="forgot-password text-right">
-        Already registered <a href="#">sign in?</a>
+        Already registered <a href="#">login?</a>
       </p>
     </form>
   );
